444c13219918fcacf31331985873d17462a1fafb
Ginji001 <38126022+Ginji001@users.noreply.github.com>
2026-09-12 10:46:18 +0900

    新配色の古いキャッシュを更新

--- dist/app.js
+++ dist/app.js
@@ -1158,7 +1158,7 @@
     installBtn.hidden = true;
   });
 
-  if ("serviceWorker" in navigator) window.addEventListener("load", () => navigator.serviceWorker.register("./sw.js"));
+  if ("serviceWorker" in navigator) window.addEventListener("load", () => navigator.serviceWorker.register("./sw.js?v=12"));
 
   function registerWebMCP() {
     const context = document.modelContext;
