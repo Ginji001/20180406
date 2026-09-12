8c1024d7a964d340f817b083ab356e071f765132
Ginji001 <38126022+Ginji001@users.noreply.github.com>
2026-09-12 14:43:10 +0900

    Refresh PWA cache for Google Calendar

--- dist/sw.js
+++ dist/sw.js
@@ -1,5 +1,5 @@
-const CACHE = "hachiroku-techo-v17";
-const ASSETS = ["./", "./index.html?v=17", "./styles.css?v=17", "./app.js?v=17", "./manifest.webmanifest?v=17", "./favicon.svg", "./icon-192.png", "./icon-512.png", "./paper-sheet.pdf", "./google-calendar.js?v=17"];
+const CACHE = "hachiroku-techo-v18";
+const ASSETS = ["./", "./index.html?v=18", "./styles.css?v=18", "./app.js?v=18", "./manifest.webmanifest?v=18", "./favicon.svg", "./icon-192.png", "./icon-512.png", "./paper-sheet.pdf", "./google-calendar.js?v=18"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS))));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))))));
 self.addEventListener("fetch", (event) => {
